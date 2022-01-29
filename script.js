595f595aa0a8ad7690c887b1cd186a92fe2a157f
stenio sousa <81321894+steniosousa@users.noreply.github.com>
2022-01-28 22:56:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function instagram() {
+    window.location.href="https://www.instagram.com/criativolivraria/";
+}
+
+function fechar(){
+    let modal = document.querySelector(".modal");
+    modal.style.display="none";
+
+}
+
+setTimeout(function() {
+    let modal = document.querySelector(".modal");
+    modal.style.display="inline";
+  }, 500);
+
+  
